e6cccce7ed0201607e6f66fb7c8f33d09e3bb3c6
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-07 21:56:11 -0300

    auteracao 2

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ from flask import Flask, request
 app = Flask(__name__)
 
 #endpoint para buscar todos os alunos
-@app.route('/buscar_alunos', methods=['GET'])
-def buscar_alunos():
-    return "todos alunos listados"
+@app.route('/buscar_alunos/<int:aluno_id>/<string:nome>', methods=['GET'])
+def buscar_alunos(aluno_id, nome):
+    return f"O aluno: {nome}, do ID: {aluno_id}, foi encontrado"
 
+
+
+
+@app.route('/adicionar_aluno', methods=['POST'])
+def adicionar_aluno():
+    alunorm = request.args.get('alunorm')
+    nome = request.args.get('nome')
+    idade = request.args.get('idade')
+    return f"O aluno: {nome}, do RM: {alunorm}, com a idade de: {idade} anos, foi adicionado a lista de alunos"
+
+
+
+
+
+@app.route('/listar_alunos', methods=['POST'])
+def listar_alunos(alunos):
+    alunos = ["Christopher", "Daniel", "Davi", "Igor"]
+    return f"Lista de alunos: {alunos}"
 
 
 
