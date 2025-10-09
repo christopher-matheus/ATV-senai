d0ee89fd55803d4895c650e243715d979beb9a6d
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-09 20:09:16 -0300

    nova auteração

--- app.py
+++ app.py
@@ -2,35 +2,35 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+alunos =[
+    {"id": 1, "nome": "João"},
+    {"id": 2, "nome": "Maria"}]
+
 #endpoint para buscar todos os alunos
 @app.route('/buscar_alunos/<int:aluno_id>/<string:nome>', methods=['GET'])
 def buscar_alunos(aluno_id, nome):
     return f"O aluno: {nome}, do ID: {aluno_id}, foi encontrado"
 
-
-
 #endpoint para adicionar um aluno
 @app.route('/adicionar_aluno', methods=['POST'])
 def adicionar_aluno():
-    alunorm = request.args.get('alunorm')
+    id= request.args.get('id')
     nome = request.args.get('nome')
-    idade = request.args.get('idade')
-    return f"O aluno: {nome}, do RM: {alunorm}, com a idade de: {idade} anos, foi adicionado a lista de alunos"
+    new = {"alunorm": id, "nome": nome}
+    new["alunorm"] = len(alunos) + 1
+    alunos.append(new)
+    return alunos
 
-
-
+def buscar_aluno_por_id(aluno_id):
+    for aluno in alunos:
+        if aluno["alunorm"] == aluno_id:
+            return aluno
+    return "Aluno não encontrado"
 
 #endpoint para listar todos os alunos
 @app.route('/listar_alunos', methods=['POST'])
 def listar_alunos(alunos):
-    alunos = ["Christopher", "Daniel", "Davi", "Igor", "Emanuel", "Gabriel Dutra", "João Filipe", "Roger"]
-    return f"Lista de alunos: {alunos}"
-
-
-
-
-
-
+    return alunos
 
 # Rodar o aplicativo Flask
 if __name__ == '__main__':
